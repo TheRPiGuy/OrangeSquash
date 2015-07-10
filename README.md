bdf195a3d71cd2ee9731ef96923773bfc94bc400
TheRPiGuy <jai.p.sharma1@gmail.com>
2015-07-10 22:18:52 +0100

    Player now executes silently, also small update to readme

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ A summer project in C++ that will hopefully be useful.
 Uses standard C++ libraries and requires mplayer to be installed and accessed at path "/usr/bin/mplayer"
 
 This project is not yet completed, but if you want to try it out, run the Makefile using the make the commnd and execute the compiled binary using "./OrangeSquash"
+
+During development of this project files will continually be changing and new files added. The main.cpp file contains some semblance to what the final program may contain but may also frequently contain temporary test code.
--- player/player.cpp
+++ player/player.cpp
@@ -1,8 +1,8 @@
 #include "player.h"
 
-#include <thread>
-#include <chrono>
 #include <unistd.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 #include <sys/wait.h>
 #include <signal.h>
 #include <iostream>
@@ -18,31 +18,31 @@ void player(Song s)
 	
 	//char* program = "/usr/bin/mplayer";
 
-	pid_t pid = fork();
+	pid_t pid_player = fork();
 	
 	char* path = &s.getPath()[0u];
 	
-	switch(pid)
+	switch(pid_player)
 	{
 		case -1: /* Error */
 			std::cerr << "Uh-Oh! fork() failed.\n";
 			exit(1);
 
 		case 0: /* Child process */
-			execl("/usr/bin/mplayer", "-really-quiet", path, NULL); /* Execute the program */
+			execl("/usr/bin/mplayer", "-slave", "-really-quiet", path, NULL); /* Execute the program */
 
 			std::cerr << "Uh-Oh! execl() failed!"; /* execl doesn't return unless there's an error */
 			exit(1);
 
 		default: /* Parent process */
-			std::cout << "Process created with pid " << pid << "\n";
+			std::cout << "Process created with pid " << pid_player << "\n";
         	
 			int status;
 			while(!WIFEXITED(status))
 			{
-				waitpid(pid, &status, 0); /* Wait for the process to complete */
+				waitpid(pid_player, &status, 0); /* Wait for the process to complete */
 			}
-			std::cout << "Process exited with " << WEXITSTATUS(status) << "\n";  	
+			std::cout << "Process exited with " << WEXITSTATUS(status) << std::endl;  	
 	}
 		
 	//Once either of these child processes had ended, kill the other
